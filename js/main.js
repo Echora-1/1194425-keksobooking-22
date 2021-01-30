8c33188a8d4c1aa3bda0f3ae60a2416cd31a19c4
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-01-30 18:21:19 +0300

    Добавил функции возвращающие случайное число из диапазона

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let getRandomIntFromRange = function (min, max) {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  if(min === max) {
+    return 'Аргументы не могут быть равны или меньше нуля';
+  }
+  if(max < min) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let getRandomFloatFromRange = function (min, max, decimal) {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  decimal = Math.abs(decimal);
+  if(min === max) {
+    return 'Аргументы не могут быть равны или меньше нуля';
+  }
+  if(max < min) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  return (Math.random() * (max - min) + min).toFixed(decimal);
+}
+
+getRandomIntFromRange(2, 5);
+getRandomFloatFromRange(1.2, 1.3, 2);
