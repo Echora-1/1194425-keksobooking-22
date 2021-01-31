b08b6158a0f707449ad3d9d851f70f98826b56fd
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-01-31 17:02:19 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,33 +1,10 @@
 'use strict'
 
-let getRandomIntFromRange = (min, max) => {
-  min = Math.abs(min);
-  max = Math.abs(max);
-  if(min === max) {
-    return 'Аргументы не могут быть равны или меньше нуля';
-  }
-  if(max < min) {
-    let temp = min;
-    min = max;
-    max = temp;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let getRandomFloatFromRange = (min, max, decimal) => {
+const getRandomFloatFromRange = (min, max, decimal = 0) => {
   min = Math.abs(min);
   max = Math.abs(max);
   decimal = Math.abs(decimal);
-  if(min === max) {
-    return 'Аргументы не могут быть равны или меньше нуля';
-  }
-  if(max < min) {
-    let temp = min;
-    min = max;
-    max = temp;
-  }
-  return (Math.random() * (max - min) + min).toFixed(decimal);
+  return Math.abs((Math.random() * (max - min) + min).toFixed(decimal));
 }
 
-getRandomIntFromRange(2, 5);
 getRandomFloatFromRange(1.2, 1.3, 2);
