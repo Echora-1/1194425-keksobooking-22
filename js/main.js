3db84c634157b18cadc224525713a8700d7a83d4
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-03-03 15:18:48 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+import {setThrottle} from './utils.js';
 import {assignInactiveStatus as assignPageInactiveStatus} from './page-states.js';
 import {load as loadMap, createAdMarkers} from './map.js';
 import {} from './form.js';
 import {getData} from './server-connection.js';
 import {setMapChanges as setMapFilterChanges} from './filter.js';
-/* global _:readonly */
 
 const RERENDER_DELAY = 500;
 
@@ -12,6 +12,6 @@ assignPageInactiveStatus();
 loadMap();
 getData((data) => {
   createAdMarkers(data);
-  setMapFilterChanges(_.throttle(() => createAdMarkers(data), RERENDER_DELAY));
+  setMapFilterChanges(setThrottle(() => createAdMarkers(data), RERENDER_DELAY));
 });
 
--- js/utils.js
+++ js/utils.js
@@ -29,4 +29,33 @@ const removeСhildByClass = (parentElement, childClass) => {
   });
 };
 
-export {getRandomFloatFromRange, getRandomArrayElement, getArrayRandomLength, removeСhildByClass};
+const setThrottle = (func, ms) => {
+  let ignore = false;
+  let lastArgs = null;
+  let lastThis = null;
+
+  const throttle = function() {
+    if(ignore) {
+      lastArgs = arguments;
+      lastThis = this;
+      return;
+    }
+
+    func.apply(this, arguments);
+
+    ignore = true;
+
+    setTimeout(() => {
+      ignore = false;
+      if(lastArgs) {
+        throttle.apply(lastThis, lastArgs);
+        lastArgs = null;
+        lastThis = null;
+      }
+    }, ms);
+  };
+
+  return throttle;
+};
+
+export {getRandomFloatFromRange, getRandomArrayElement, getArrayRandomLength, removeСhildByClass, setThrottle};
