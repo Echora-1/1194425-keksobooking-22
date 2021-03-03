4794e4688a6208aabcc98dbeafff3e9e912034b6
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-03-03 09:57:39 +0300

    Применил «устранение дребезга» для отрисовки маркеров

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ import {load as loadMap, createAdMarkers} from './map.js';
 import {} from './form.js';
 import {getData} from './server-connection.js';
 import {setMapChanges as setMapFilterChanges} from './filter.js';
+/* global _:readonly */
+
+const RERENDER_DELAY = 500;
 
 assignPageInactiveStatus();
 
 loadMap();
 getData((data) => {
   createAdMarkers(data);
-  setMapFilterChanges(() => createAdMarkers(data));
+  setMapFilterChanges(_.throttle(() => createAdMarkers(data), RERENDER_DELAY));
 });
 
