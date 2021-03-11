5a5448c32c6b1c3051844d6f2d454f3a8195866a
Echora-1 <51864493+Echora-1@users.noreply.github.com>
2021-03-11 16:22:07 +0300

    Удалил не используемый код

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,26 +1,3 @@
-const getRandomFloatFromRange = (min, max, decimal = 0) => {
-  min = Math.abs(min);
-  max = Math.abs(max);
-  decimal = Math.abs(decimal);
-
-  return Math.abs((Math.random() * (max - min) + min).toFixed(decimal));
-};
-
-const getRandomArrayElement = (array) => {
-  return array[getRandomFloatFromRange(0, array.length - 1)];
-};
-
-const getArrayRandomLength = (array) => {
-  const randomLength = getRandomFloatFromRange(0, array.length - 1);
-  const arrayElements = [];
-
-  for(let i = 0; i <= randomLength; i++) {
-    arrayElements.push(array[i]);
-  }
-
-  return arrayElements;
-};
-
 const removeСhildByClass = (parentElement, childClass) => {
   const arrayChildren = Array.from(parentElement.children);
   arrayChildren.forEach((element) => {
@@ -58,4 +35,4 @@ const setThrottle = (func, ms) => {
   return throttle;
 };
 
-export {getRandomFloatFromRange, getRandomArrayElement, getArrayRandomLength, removeСhildByClass, setThrottle};
+export {removeСhildByClass, setThrottle};
